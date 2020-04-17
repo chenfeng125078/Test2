0a7ff5dfd485e1bf7fec598bca77da255392bbb1
chenfeng <1250783502@qq.com>
2020-04-17 11:19:29 +0800

    add 3.py

---
+++ test_3333.py
@@ -0,0 +1,13 @@
+class A:
+    def __init__(self):
+        print("this is class A")
+
+class B:
+    def __init__(self):
+        print('this is class B')
+
+
+if __name__ == "main":
+    a = A()
+    b = B()
+
