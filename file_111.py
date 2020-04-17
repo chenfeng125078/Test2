44183b598a5cd65988b94c0e99b757f98a9ce7a0
chenfeng <1250783502@qq.com>
2020-04-17 09:27:48 +0800

    two files

---
+++ file_111.py
@@ -0,0 +1,11 @@
+import numpy
+
+
+class A:
+    def __init__(self):
+        print("this is the first file A class")
+        print("no change")
+
+
+if __name__ == '__main__':
+    printer = A()
---
+++ file_222.py
@@ -0,0 +1,18 @@
+import numpy
+
+
+class A:
+    def __init__(self):
+        print("this is second file A class")
+        print("no change")
+
+
+class B:
+    def __init__(self):
+        print("this is second file B class")
+        print("no change")
+
+
+if __name__ == '__main__':
+    printer_1 = A()
+    printer_2 = B()
